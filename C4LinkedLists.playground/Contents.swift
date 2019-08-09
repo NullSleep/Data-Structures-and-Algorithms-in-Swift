91453852d7cd33591206955585b90938c1be70b2
Carlos Arenas <nullsleep@me.com>
2019-08-08 22:04:24 -0500

    Creating nodes and some documentation

--- C4LinkedLists.playground/Contents.swift
+++ C4LinkedLists.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var str = "Hello, playground"
+// A linked list is a collection of values arranged in a linear unidirectional sequence. A linked list has several theoretical advantages over contiguous storage options such as the Swift Array: Constant time insertion and removel from the front of the list; Reliable performance characteristics.
+
+// NODES
+
+example(of: "Creating and linking nodes") {
+  let node1 = Node(value: 1)
+  let node2 = Node(value: 2)
+  let node3 = Node(value: 3)
+  
+  node1.next = node2
+  node2.next = node3
+  
+  print(node1)
+}
