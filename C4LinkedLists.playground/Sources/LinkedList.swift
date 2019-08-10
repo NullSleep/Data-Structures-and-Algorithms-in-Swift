04fda2e87f6c55d815727e8a6a5cd61841cd9120
Carlos Arenas <nullsleep@me.com>
2019-08-10 16:56:23 -0500

    Fixing a compiler error and adding append operations

--- C4LinkedLists.playground/Sources/LinkedList.swift
+++ C4LinkedLists.playground/Sources/LinkedList.swift
@@ -10,7 +10,7 @@ public struct LinkedList<Value> {
   public init() {}
   
   public var isEmpty: Bool {
-    return head = nil
+    return head == nil
   }
   
   // Adding a value at the front of the list is known as a push operation. This is also known as head-first insertion.
@@ -21,6 +21,22 @@ public struct LinkedList<Value> {
       tail = head
     }
   }
+  
+  // Append operations. This is meant to add a value at the end of the list, and it is known as tail-end insertion.
+  public mutating func append(_ value: Value) {
+    // If the list is emoty, you'll need to update both head and tail to the new node. Since append on an empty list is functionally indentical to push, you simply invoke push to do the work.
+    guard !isEmpty else {
+      push(value)
+      return
+    }
+    
+    // In all other cases, you simply create a new node after the tail node. Force unwrapping is guaranteed to succed since you push in the isEmpty case with the above guard statement.
+    tail!.next = Node(value: value)
+    
+    // Since this is tail-end insertion, your new node is also the tail of the list.
+    tail = tail!.next
+  }
+  
 }
 
 extension LinkedList: CustomStringConvertible {
@@ -29,6 +45,6 @@ extension LinkedList: CustomStringConvertible {
     guard let head = head else {
       return "Empty List"
     }
+    return String(describing: head)
   }
-  return String(describing: head)
 }
